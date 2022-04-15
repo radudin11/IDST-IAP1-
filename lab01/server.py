79d870369ae77c392a083dbf604930eb04cab83c
Dinicescu Radu <rodede.radu@gmail.com>
2022-04-15 15:01:23 +0300

    added lab02

--- lab01/server.py
+++ lab01/server.py
@@ -65,23 +65,24 @@ def loggedin():
 
 @app.route("/upload.html", methods = ['GET', 'POST'])
 def upload():
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # If the user does not select a file, the browser submits an
-        # empty file without a filename.
-        if file.filename == '':
-            flash('No selected file')
-            return render_template("upload.html")
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect("upload.html")
-    return render_template("upload.html")
-
+    if 'username' in session:
+        if request.method == 'POST':
+            # check if the post request has the file part
+            if 'file' not in request.files:
+                flash('No file part')
+                return redirect(request.url)
+            file = request.files['file']
+            # If the user does not select a file, the browser submits an
+            # empty file without a filename.
+            if file.filename == '':
+                flash('No selected file')
+                return render_template("upload.html")
+            if file and allowed_file(file.filename):
+                filename = secure_filename(file.filename)
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                return redirect("upload.html")
+        return render_template("upload.html")
+    return render_template("login.html", error_msg="You have to be logged in")
 if __name__ == "__main__":
     loggedin = False
     app.run(debug=True)
---
+++ lab02/fb2img.py
@@ -0,0 +1,47 @@
+#!.venv/bin/python3
+
+import argparse         # argument parsing
+import struct           # data unpacking
+from PIL import Image   # image processing
+
+def main():
+    # parse cli arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('FILE', help='output image file')
+    parser.add_argument('--src', help='data source',
+                        default='/dev/fb0', metavar='/dev/fb*')
+    parser.add_argument('--width', help='screen width [px]',
+                        type=int, default=1920, metavar=' INT')
+    parser.add_argument('--height', help='screen height [px]',
+                        type=int, default=1080, metavar='INT')
+    cfg = parser.parse_args()
+
+    # TODO 1: read contents of cfg.src (the frame buffer)
+    print("hello")
+    print(cfg.src)
+    frameBuff = open(cfg.src, "rb")
+    fileContent = frameBuff.read()
+    print(fileContent)
+    # TODO 2: split data in groups of 4 bytes
+    
+    # create a new PIL Image object
+    img = Image.new('RGB', (cfg.width, cfg.height))
+    px  = img.load()
+
+    # set each pixel value
+    for i in range(cfg.width):
+        for j in range(cfg.height):
+            # TODO 3: write each pixel value in px[i,j] as a RGB tuple
+            # NOTE  : the four bytes in the groups that you split previously
+            #         are in fact in BGRA format; we don't need the Alpha
+            #         value but the other three bytes must be revered
+            pass
+    # save image do disk
+    # NOTE: format will be determined from the file's extension
+    #img.save(cfg.FILE, None)
+
+    # clean up PIL Image object
+    img.close()
+
+if __name__ == '__main__':
+    main()
